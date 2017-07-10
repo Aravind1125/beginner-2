82c67789f8aa368e857b81fd3d76707105884875
Aravind1125 <aravindkumar_ravi@srmuniv.edu.in>
2017-07-11 00:16:00 +0530

    Add files via upload

---
+++ leap year or not.java
@@ -0,0 +1,18 @@
+public class java
+{
+public static void main(String[] agrs);
+{
+int x;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the year ....");
+x = s.nextInt();
+if ( x % 4 == 0 )
+{
+System.out.println("it's leap year");
+}
+else
+{
+System.out.println("it's not leap year");
+}
+}
+}
