13cef58ce49ecf6f9b52b378a1ef515b3a18548e
Aravind1125 <aravindkumar_ravi@srmuniv.edu.in>
2017-07-10 22:42:35 +0530

    Add files via upload

---
+++ to find odd or even.java
@@ -0,0 +1,18 @@
+public class odd or even
+{
+public static void main(String[] agrs);
+{
+int a;
+Scanner s = new Scanner(System.in);
+System.out.println("check the num is ODD or EVEN..Enter the num..");
+a = s.nextInt();
+if ( a%2 == 0 )
+{
+System.out.println("EVEN num");
+}
+else
+{
+System.out.println("ODD num");
+}
+}
+}
