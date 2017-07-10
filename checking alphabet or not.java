943b14d96d1676faa74249ea5e044338fde42d95
Aravind1125 <aravindkumar_ravi@srmuniv.edu.in>
2017-07-10 23:29:42 +0530

    Add files via upload

---
+++ checking alphabet or not.java
@@ -0,0 +1,18 @@
+public class odd or even
+{
+public static void main(String[] agrs);
+{
+char a;
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the Character..");
+a = s.next().charAt(0);
+if ( a >= 'A' && a <= 'Z')
+{
+System.out.println("this is alphabet");
+}
+else
+{
+System.out.println("this is not alphabet");
+}
+}
+}
