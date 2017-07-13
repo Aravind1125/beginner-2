93e44f673504c60f2ec4c15444e57da30918f0ec
Aravind1125 <aravindkumar_ravi@srmuniv.edu.in>
2017-07-14 00:47:06 +0530

    Add files via upload

---
+++ Even num between two intervals.java
@@ -0,0 +1,17 @@
+	class java 
+       {
+       
+	public static void main (String[] args)
+        { 
+        int i,x;
+	int sum ;
+	Scanner s = new Scanner(System.in);
+	x = s.nextInt();
+	System.out.println("Even number between 1 and " +x);
+	for ( i = 1; i <= (x/2); i++ )
+	{
+        sum = (2*i);
+        System.out.println(sum);
+        }
+      }
+}
